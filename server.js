d6dc7acb9b1640a07a2b6067f60dea2ee694ff52
Mateoc1 <mateo.chazarreta.et21.21@gmail.com>
2026-06-19 15:56:38 -0300

    ñeñeñeñeñe

--- server.js
+++ server.js
@@ -1,32 +1,21 @@
 require("dotenv").config();
 
 const express = require("express");
-const path = require("path");
-console.log("Sirviendo estáticos desde:", path.join(__dirname, "../frontend/pages"));
+const cors = require("cors");
 
-// Routers
-const hotelesRouter = require("./routes/hoteles");
-const checkoutRouter = require('./routes/checkout');
+const hotelesRouter = require("./src/routes/hoteles");
+const checkoutRouter = require("./src/routes/checkout");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../frontend")));
-app.use(express.static(path.join(__dirname, "../frontend/pages")));
-
-
-// Endpoints
 app.use("/api/hoteles", hotelesRouter);
 app.use("/api/checkout", checkoutRouter);
 
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log('http://localhost:3000/index.html - Menu');
-    console.log('http://localhost:3000/admin.html - Administrador');
     console.log(`Servidor iniciado en puerto ${PORT}`);
 });
-
-//
